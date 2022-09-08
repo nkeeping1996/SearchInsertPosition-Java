86352de5ad8d2f89fe1d9be197063bb4f73a770b
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-08 16:25:10 -0400

    Reducing iterations of for loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {2,3,7,9};
+        int target = 4;
+
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        if(nums[0]>=target){
+            return 0;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target || nums[i]>target){
+                return i;
+            }
+        }
+
+        return nums.length;
+
     }
 }
